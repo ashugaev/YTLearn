0f149ef8a547b15786e9cf746dc463ddf363d1a1
Shugaev <alek-2020@yande.ru>
2018-09-09 19:56:42 +0300

    .

--- src/js/components/MyTestComponent.js
+++ src/js/components/MyTestComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import UserInfo from "./UserInfo";
 
+const wrapper = document.getElementById("root");
+
 
 // Компонент 1
 class Body extends Component {
@@ -59,8 +61,112 @@ class Body2 extends Component {
   }
 }
 
-export default Body;
+// Компонент 3
+let app2 = {
+  counter: 0
+}
 
-// Получаем dom element в который будем вставлять контент
-const wrapper = document.getElementById("root");
-ReactDOM.render(<Body2 />, wrapper);
+let counter = 0;
+
+const addOne = () => {counter++; renderCounterApp();}
+const resetCounrer = () => {counter = 0; renderCounterApp();}
+const minusOne = () => {counter--; renderCounterApp();}
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const option = event.target.elements.option.value
+  const optionInt = parseInt(option);
+  if(optionInt) {
+    counter += optionInt;
+    renderCounterApp();
+    console.log(option)
+  }
+}
+
+const renderCounterApp = () => {
+  class Body3 extends Component {
+    render() {
+      return (
+        <div>
+          <button onClick={addOne}>Conter++</button>
+          <button onClick={minusOne}>Conter--</button>
+          <button onClick={resetCounrer}>reset</button>  
+
+          <form onSubmit={onFormSubmit}>
+            <input type="text" name="option" />
+            <input type="submit" style={{marginTop: '10px'}} value="Add to counter" />
+          </form>
+
+          <h1 className="className">{counter}</h1>
+        </div>
+      )
+    }
+  }
+
+  // Получаем dom element в который будем вставлять контент
+  const wrapper = document.getElementById("root");
+  ReactDOM.render(<Body3 />, wrapper);
+}
+
+// renderCounterApp();
+
+
+
+// Компонент 4
+let listOfItems = [];
+let visibility = true;
+
+const addToList = (event) => {
+   const value = event.target.elements.option.value
+   listOfItems.push(value); 
+   renderCounterApp2()
+}
+
+const clearList = () => {
+  listOfItems = []
+  renderCounterApp2()
+}
+
+const chooseTask = () => {
+  const randIndex = Math.floor(Math.random() * listOfItems.length )
+  const option = listOfItems[randIndex]
+  console.log(option)
+}
+
+const hideAll = () => {
+  visibility = !visibility
+  renderCounterApp2()
+}
+
+const renderCounterApp2 = () => {
+  class Body4 extends Component {
+    render() {
+      return (
+        <div>
+             <button disabled={listOfItems.length === 0} onClick={chooseTask}>Чем заняться?</button>
+             {(listOfItems.length > 0) ? 'Список дел:' : 'Отдохнем немножечко'} {(listOfItems.length > 0) && listOfItems.length}
+             {visibility && 
+              <ol>
+                {
+                  listOfItems.map((item) => <li key={item}>{item}</li>)
+                }
+              </ol>
+             }
+             <form onSubmit={addToList}>
+               <input name="option" type="text"/>
+               <input type="submit" value="Добавить в список" />
+             </form>
+             <button onClick={clearList}>Почистить</button>
+             <button onClick={hideAll}>{ visibility ? 'Спрятать все' : 'Показать все'}</button>
+
+        </div>
+      )
+    }
+  }
+
+  // Получаем dom element в который будем вставлять контент
+  ReactDOM.render(<Body4 />, wrapper);
+
+}
+
+renderCounterApp2()
